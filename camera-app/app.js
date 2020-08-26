4356189b8cc6dcedcbe1a91853c3ca2e6331b8bb
VinNolden1 <45859399+VinNolden1@users.noreply.github.com>
2020-08-26 11:14:54 -0500

    Add files via upload

---
+++ camera-app/app.js
@@ -0,0 +1,28 @@
+let contsraints = { video: {facingMode: "user "}, audio: false };
+
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(contsraints)
+        .then(function(stream) {
+            track = stream.getTracks() [0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Something is Broken.", error)
+        });
+}
+
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = camerav.videoHeight;
+    cameraSensor.getContext ("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataUrl("image/webp");
+    cameraOutput.classList.add('taken');
+};
+
+window.addEventListener("load". cameraStart, false);
